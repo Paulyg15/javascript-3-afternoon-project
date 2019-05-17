ca05c0abf2d3c1eda1c264a75a9de3eaf184b328
Paul Graves <pgraves@hsahealthplan.com>
2019-05-16 19:18:47 -0600

    Finished the problems  of the 5 js files

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,12 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  var arr = [];
+
+  for(var prop in obj) {
+    arr.push(obj[prop]);
+  }
+  return arr.join("");
 }
 
 
@@ -53,7 +58,14 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function greaterThan10(obj) {
+  for(var num in obj) {
+    if (obj[num] > 10) {
+      obj[num] = 0;
+    }
+  }
+  return obj;
+}
 
 
 
@@ -65,7 +77,12 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+function double(obj) {
+  for(var num in obj) {
+    obj[num] *= 2;
+  }
+  return obj;
+}
 
 
 
@@ -79,7 +96,16 @@ function showValues( obj ) {
   By the end of the for in loop, you should have a sentence, return that sentence.
 */
 
-//Code Here
+function secrets(obj) {
+  var str = "";
+
+  for(var prop in obj) {
+    if (prop.startsWith("sh")) {
+      str += obj[prop];
+    }
+  }
+  return str;
+}
 
 
 
@@ -110,7 +136,14 @@ function showValues( obj ) {
   Delete the property password and return the object.
 */
 
-//Code Here
+function removePassword(obj) {
+  for(var key in obj) {
+    if (key == "password") {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 
@@ -129,7 +162,11 @@ var deleteTheBigNumbers = {
   Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
 */
 
-//Code Here
+for(var key in deleteTheBigNumbers) {
+  if (deleteTheBigNumbers[key] > 100) {
+    delete deleteTheBigNumbers[key];
+  }
+}
 
 
 
@@ -142,7 +179,14 @@ var deleteTheBigNumbers = {
   Return the updated object.
 */
 
-//Code Here
+function startsWithK(obj) {
+  for(var key in obj) {
+    if (key.startsWith("k")) {
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 
@@ -157,6 +201,13 @@ var deleteTheBigNumbers = {
   (hint: the method includes() may be of use...)
 */
 
-//Code Here
+function hiddenTreasure(obj) {
+  for(var sentence in obj) {
+    if (!obj[sentence].includes("treasure")) {
+      delete obj[sentence];
+    }
+  }
+  return obj;
+}
 
 
--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(num => num % 2 == 0); 
+ // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
 
@@ -43,8 +43,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+
+let postTaxPrices = prices.map(x => x * 1.07);
+ // = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -62,8 +63,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
   (runningTotal, curElement, curIndex, wholeArray)=>{} Arrow Form
 */
 
-//Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+
+let totalPopulation = populations.reduce((x, y) => x + y);
+ //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -88,8 +90,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-//Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+
+let myStrongest = monstersInYourPocket.filter(x => x.CP > 200);
+ // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
@@ -106,7 +109,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(x => x.price * (1 + x.tax));
 
 
 
@@ -126,6 +129,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(x => x.owner == "Bob").reduce(function(a, b) {
+  return a + b.price;
+}, 0);
 
 
